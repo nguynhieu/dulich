c3c065148a8c0ae128e6e8758503b2a1272ceb45
Nguyen Hieu <tthzicc@gmail.com>
2021-10-30 16:30:56 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -22,6 +27,7 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/offers" component={Offers} />
           <Route path="/about" component={About} />
+          <Redirect to="/" />
         </Switch>
         <Footer />
         <GlobalStyle />
